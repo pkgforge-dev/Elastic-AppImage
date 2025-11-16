dd2993960cb2ec383783a243fd00f998adad1fa5
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2025-11-16 10:09:49 +0100

    Migrate to new generic AppImage template

---
+++ make-appimage.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+set -eu
+
+ARCH=$(uname -m)
+VERSION=$(pacman -Q elastic | awk '{print $2; exit}')
+export ARCH VERSION
+export OUTPATH=./dist
+export ADD_HOOKS="self-updater.bg.hook"
+export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
+export ICON=/usr/share/icons/hicolor/scalable/apps/app.drey.Elastic.svg
+export DESKTOP=/usr/share/applications/app.drey.Elastic.desktop
+export DEPLOY_OPENGL=1
+export STARTUPWMCLASS=elastic # For Wayland, this is 'app.drey.Elastic', so this needs to be changed in desktop file manually by the user in that case until some potential automatic fix exists for this
+
+# Trace and deploy all files and directories needed for the application (including binaries, libraries and others)
+quick-sharun /usr/bin/elastic
+
+# Turn AppDir into AppImage
+quick-sharun --make-appimage
