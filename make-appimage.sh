3d74e80d517535feeb7be72b41763d50b9c85cbf
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2025-12-20 10:40:53 +0100

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -11,7 +11,6 @@ export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}
 export ICON=/usr/share/icons/hicolor/scalable/apps/app.drey.Elastic.svg
 export DESKTOP=/usr/share/applications/app.drey.Elastic.desktop
 export DEPLOY_OPENGL=1
-export STARTUPWMCLASS=elastic
 export GTK_CLASS_FIX=1
 
 # Trace and deploy all files and directories needed for the application (including binaries, libraries and others)
